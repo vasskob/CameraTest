9fa8424e7b8f12703e0ea4cd22f47f6c96227f1b
vasskob <vasskob@gmail.com>
2017-08-28 18:27:02 +0300

    Add camera pause & release . Add auto focus.

--- app/src/main/java/com/example/vasskob/mycamera/CameraActivity.java
+++ app/src/main/java/com/example/vasskob/mycamera/CameraActivity.java
@@ -1,11 +1,14 @@
 package com.example.vasskob.mycamera;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.app.Activity;
 import android.content.Context;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Surface;
 import android.view.View;
@@ -21,20 +24,20 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
-public class CameraActivity extends AppCompatActivity {
+public class CameraActivity extends Activity {
 
     @BindView(R.id.main_container)
     ViewGroup mRootView;
-
     @BindView(R.id.camera_preview)
     ViewGroup preview;
 
-
+    private static final long UI_ANIMATION_DELAY = 1000;
     private static final String TAG = CameraActivity.class.getSimpleName();
 
+    private final Handler mHideHandler = new Handler();
     private Camera mCamera;
     private CameraPreview mPreview;
-
+    private View decorView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -48,25 +51,79 @@ public class CameraActivity extends AppCompatActivity {
     }
 
     protected void makeActivityFullScreen() {
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        decorView = getWindow().getDecorView();
+        makeFullScreen();
+        decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                    mHideHandler.postDelayed(mHideRunnable, UI_ANIMATION_DELAY);
+                }
+            }
+        });
+
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
     }
+
+    private void makeFullScreen() {
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    private final Runnable mHideRunnable = new Runnable() {
+        @SuppressLint("InlinedApi")
+        @Override
+        public void run() {
+            makeFullScreen();
+        }
+    };
 
     private void addCameraPreview() {
         // Create an instance of Camera
         mCamera = getCameraInstance();
-        setCameraDisplayOrientation(Camera.getNumberOfCameras()-1, mCamera);
+        setCameraDisplayOrientation(Camera.getNumberOfCameras() - 1, mCamera);
+        setCameraAutoFocus(mCamera);
+        //     setFaceDetection(mCamera);
+
         // Create our Preview view and set it as the content of our activity.
         mPreview = new CameraPreview(this, mCamera);
         preview.addView(mPreview);
+        Log.d(TAG, "addCameraPreview: ");
+    }
 
+//    private void setFaceDetection(Camera mCamera) {
+//        mCamera.setFaceDetectionListener(new Camera.FaceDetectionListener() {
+//            @Override
+//            public void onFaceDetection(Camera.Face[] faces, Camera camera) {
+//
+//            }
+//        });
+//        Camera.Parameters params = mCamera.getParameters();
+//
+//        // start face detection only *after* preview has started
+//        if (params.getMaxNumDetectedFaces() > 0){
+//            // camera supports face detection, so can start it:
+//            mCamera.startFaceDetection();
+//        }
+//    }
+
+    private void setCameraAutoFocus(Camera c) {
+        Camera.Parameters params = c.getParameters();
+        if (params.getSupportedFocusModes().contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
+            params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+        }
+        c.setParameters(params);
     }
 
     private Camera getCameraInstance() {
         Camera c = null;
         try {
-            c = Camera.open(Camera.getNumberOfCameras()-1); // attempt to get a Camera instance
+            c = Camera.open(Camera.getNumberOfCameras() - 1); // attempt to get a Camera instance
         } catch (Exception e) {
             // Camera is not available (in use or does not exist)
             Toast.makeText(this, getString(R.string.cameraWarn), Toast.LENGTH_SHORT).show();
@@ -86,10 +143,18 @@ public class CameraActivity extends AppCompatActivity {
         int degrees = 0;
 
         switch (rotation) {
-            case Surface.ROTATION_0: degrees = 0; break;
-            case Surface.ROTATION_90: degrees = 90; break;
-            case Surface.ROTATION_180: degrees = 180; break;
-            case Surface.ROTATION_270: degrees = 270; break;
+            case Surface.ROTATION_0:
+                degrees = 0;
+                break;
+            case Surface.ROTATION_90:
+                degrees = 90;
+                break;
+            case Surface.ROTATION_180:
+                degrees = 180;
+                break;
+            case Surface.ROTATION_270:
+                degrees = 270;
+                break;
         }
 
         int result;
@@ -102,6 +167,38 @@ public class CameraActivity extends AppCompatActivity {
         camera.setDisplayOrientation(result);
     }
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mHideHandler.removeCallbacks(mHideRunnable);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mCamera == null) {
+            checkPermissions();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseCamera();
+    }
+
+    private void releaseCamera() {
+        if (mCamera != null) {
+            mCamera.setPreviewCallbackWithBuffer(null);
+            preview.removeView(mPreview);
+            //mPreview.getHolder().removeCallback(mPreview);
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+            mPreview = null;
+
+        }
+    }
 
     //////////////////////// PERMISSIONS /////////////////////////
 
