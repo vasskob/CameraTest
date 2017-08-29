fb4cb9456398f7defe4fafd67926b90f1a858df1
vasskob <vasskob@gmail.com>
2017-08-29 18:36:03 +0300

    Add switch camera, save file & thumbnails.

--- app/src/main/java/com/example/vasskob/mycamera/CameraActivity.java
+++ app/src/main/java/com/example/vasskob/mycamera/CameraActivity.java
@@ -6,8 +6,16 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Drawable;
 import android.hardware.Camera;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
@@ -16,11 +24,23 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+import com.example.vasskob.mycamera.utils.CameraUtils;
+import com.mikhaellopez.circularimageview.CircularImageView;
 import com.nabinbhandari.android.permissions.PermissionHandler;
 import com.nabinbhandari.android.permissions.Permissions;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import butterknife.BindView;
@@ -31,9 +51,10 @@ import static android.hardware.Camera.Parameters.FLASH_MODE_AUTO;
 import static android.hardware.Camera.Parameters.FLASH_MODE_OFF;
 import static android.hardware.Camera.Parameters.FLASH_MODE_ON;
 import static android.hardware.Camera.Parameters.FLASH_MODE_TORCH;
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
 
 
-public class CameraActivity extends Activity {
+public class CameraActivity extends Activity implements Camera.PictureCallback, Camera.ShutterCallback {
 
     @BindView(R.id.main_container)
     ViewGroup mRootView;
@@ -44,17 +65,21 @@ public class CameraActivity extends Activity {
     @BindView(R.id.btn_flash)
     Button btnFlash;
 
+    @BindView(R.id.iv_thumbnail)
+    CircularImageView ivThumbnail;
+
     private static final String TAG = CameraActivity.class.getSimpleName();
     private static final long UI_ANIMATION_DELAY = 1000;
 
     private final Handler mHideHandler = new Handler();
-    private final Handler switchFlashHandler = new Handler();
+    private final Handler switchHandler = new Handler();
     private Camera mCamera;
     private CameraPreview mPreview;
     private View decorView;
     private Camera.Parameters params;
     private Drawable background;
     private String flashMode;
+    private SensorManager sensorManager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -63,7 +88,7 @@ public class CameraActivity extends Activity {
         ButterKnife.bind(this);
 
         makeActivityFullScreen();
-        checkPermissions();
+        checkPermissions(0);
     }
 
     protected void makeActivityFullScreen() {
@@ -73,7 +98,7 @@ public class CameraActivity extends Activity {
             @Override
             public void onSystemUiVisibilityChange(int visibility) {
                 if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
-                    mHideHandler.postDelayed(mHideRunnable, UI_ANIMATION_DELAY);
+                    mHideHandler.postDelayed(mHideStatusBar, UI_ANIMATION_DELAY);
                 }
             }
         });
@@ -91,7 +116,7 @@ public class CameraActivity extends Activity {
                         | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 
-    private final Runnable mHideRunnable = new Runnable() {
+    private final Runnable mHideStatusBar = new Runnable() {
         @SuppressLint("InlinedApi")
         @Override
         public void run() {
@@ -110,9 +135,18 @@ public class CameraActivity extends Activity {
         }
     };
 
-    private void addCameraPreview() {
-        mCamera = getCameraInstance();
+    private void addCameraPreview(int cameraId) {
+        mCamera = CameraUtils.getCameraInstance(cameraId);
+        if (mCamera == null) {
+            Toast.makeText(this, getString(R.string.cameraWarn), Toast.LENGTH_SHORT).show();
+            return;
+        }
         mCamera.setDisplayOrientation(90);
+        if (mCamera.getParameters().getSupportedFlashModes() == null) {
+            btnFlash.setVisibility(View.GONE);
+        } else {
+            btnFlash.setVisibility(View.VISIBLE);
+        }
         setCameraAutoFocus();
         setCameraDefaultFlashMode();
         Log.d(TAG, "setCameraDefaultFlashMode: " + mCamera.getParameters().getSupportedFlashModes());
@@ -123,7 +157,7 @@ public class CameraActivity extends Activity {
 
     private void setCameraDefaultFlashMode() {
         params = mCamera.getParameters();
-        if (params.getSupportedFlashModes().contains(FLASH_MODE_AUTO)) {
+        if (params.getSupportedFlashModes() != null && params.getSupportedFlashModes().contains(FLASH_MODE_AUTO)) {
             params.setFlashMode(FLASH_MODE_AUTO);
         }
         mCamera.setParameters(params);
@@ -137,16 +171,6 @@ public class CameraActivity extends Activity {
         mCamera.setParameters(params);
     }
 
-    private Camera getCameraInstance() {
-        Camera c = null;
-        try {
-            c = Camera.open(0);
-        } catch (Exception e) {
-            Toast.makeText(this, getString(R.string.cameraWarn), Toast.LENGTH_SHORT).show();
-            Log.e(TAG, "getCameraInstance: ", e);
-        }
-        return c;
-    }
 
     private int clickCounter = 1;
 
@@ -184,20 +208,88 @@ public class CameraActivity extends Activity {
             params.setFlashMode(FLASH_MODE_OFF);
             mCamera.setParameters(params);
         }
-        switchFlashHandler.postDelayed(switchFlashMode, 100);
+        switchHandler.postDelayed(switchFlashMode, 100);
+    }
+
+    @OnClick(R.id.btn_capture)
+    protected void onCaptureBtnClick() {
+        mCamera.takePicture(this, null, this);
+        getOrientation();
+
+    }
+
+    private void removeListener() {
+        sensorManager.unregisterListener(listener);
+    }
+
+    public void getOrientation() {
+        sensorManager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
+        sensorManager.registerListener(listener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
+
+    }
+
+    private SensorEventListener listener = new SensorEventListener() {
+        int orientation = -1;
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if (event.values[1] < 6.5 && event.values[1] > -6.5) {
+                if (orientation != 1) {
+                    Log.d("Sensor", "Landscape");
+                    screenOrientation = 1;
+                }
+                orientation = 1;
+            } else {
+                if (orientation != 0) {
+                    Log.d("Sensor", "Portrait");
+                    screenOrientation = 0;
+                }
+                orientation = 0;
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // TODO Auto-generated method stub
+
+        }
+    };
+
+    private int screenOrientation;
+
+
+    @BindView(R.id.btn_camera_switch)
+    ImageView ivCameraSwitch;
+    private int switchCounter;
+
+    @OnClick(R.id.btn_camera_switch)
+    protected void onSwitchCameraClick() {
+        Drawable vector = ivCameraSwitch.getDrawable();
+        ((Animatable) vector).start();
+        switchCounter++;
+        switchHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                releaseCamera();
+                checkPermissions(switchCounter);
+            }
+        }, 200);
+        if (switchCounter == Camera.getNumberOfCameras()) {
+            switchCounter = 0;
+        }
     }
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        mHideHandler.removeCallbacks(mHideRunnable);
+        mHideHandler.removeCallbacks(mHideStatusBar);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         if (mCamera == null) {
-            checkPermissions();
+            checkPermissions(0);
         }
     }
 
@@ -219,9 +311,71 @@ public class CameraActivity extends Activity {
         }
     }
 
+    @Override
+    public void onPictureTaken(byte[] data, Camera camera) {
+        File pictureFile = CameraUtils.getOutputMediaFile(MEDIA_TYPE_IMAGE);
+        if (pictureFile == null) {
+            Log.d(TAG, "Error creating media file, check storage permissions: ");
+            return;
+        }
+        try {
+            FileOutputStream fos = new FileOutputStream(pictureFile);
+            Bitmap realImage = BitmapFactory.decodeByteArray(data, 0, data.length);
+            Log.d(TAG, "onCaptureBtnClick: orientation = " + screenOrientation);
+            if (screenOrientation == 0) {
+                realImage = rotate(realImage, -90);
+            }
+            realImage.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.close();
+            removeListener();
+            mCamera.startPreview();
+            showThumbnails(pictureFile);
+        } catch (FileNotFoundException e) {
+            Log.d(TAG, "File not found: " + e.getMessage());
+        } catch (IOException e) {
+            Log.d(TAG, "Error accessing file: " + e.getMessage());
+        }
+    }
+
+    public static Bitmap rotate(Bitmap bitmap, int degree) {
+        Log.d(TAG, "rotate: = " + degree);
+        int w = bitmap.getWidth();
+        int h = bitmap.getHeight();
+
+        Matrix mtx = new Matrix();
+        //       mtx.postRotate(degree);
+        mtx.setRotate(degree);
+
+        return Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, true);
+    }
+
+    private void showThumbnails(final File pictureFile) {
+        Glide.with(this)
+                .load(pictureFile)
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        Toast.makeText(CameraActivity.this, String.format(getString(R.string.success_save_warn), pictureFile), Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                })
+                .into(ivThumbnail)
+                .onLoadFailed(ContextCompat.getDrawable(this, R.drawable.picture));
+    }
+
+    @Override
+    public void onShutter() {
+        Toast.makeText(this, getString(R.string.success_warn), Toast.LENGTH_SHORT).show();
+    }
+
     //////////////////////// PERMISSIONS /////////////////////////
 
-    private void checkPermissions() {
+    private void checkPermissions(final int cameraId) {
         Permissions.check(this, new String[]{Manifest.permission.CAMERA,
                         Manifest.permission.WRITE_EXTERNAL_STORAGE,
                         Manifest.permission.RECORD_AUDIO},
@@ -233,7 +387,7 @@ public class CameraActivity extends Activity {
                     @Override
                     public void onGranted() {
                         Log.d(TAG, "onGranted: !!!");
-                        addCameraPreview();
+                        addCameraPreview(cameraId);
                     }
 
                     @Override
---
+++ app/src/main/java/com/example/vasskob/mycamera/utils/CameraUtils.java
@@ -0,0 +1,52 @@
+package com.example.vasskob.mycamera.utils;
+
+import android.hardware.Camera;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
+
+public class CameraUtils {
+
+    private static final String TAG = CameraUtils.class.getSimpleName();
+
+    public static Camera getCameraInstance(int cameraId) {
+        Camera c = null;
+        try {
+            c = Camera.open(cameraId);
+        } catch (Exception e) {
+            Log.e(TAG, "getCameraInstance: ", e);
+        }
+        return c;
+    }
+
+    public static File getOutputMediaFile(int type) {
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_" + timeStamp + ".jpg");
+        } else if (type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_" + timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+        return mediaFile;
+    }
+}
